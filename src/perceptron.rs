eae0e4fbcea0363c0402a72859d2864c9aee8e3f
andreas <Andreas.Zwander@gmail.com>
2026-04-27 18:59:28 +0200

    Refactor `Perceptron` struct to encapsulate weights and bias using `PerceptronParameters`

--- src/perceptron.rs
+++ src/perceptron.rs
@@ -1,9 +1,9 @@
 pub use crate::activation::Activation;
 pub use crate::optimizer::Optimizer;
+use crate::parameters::PerceptronParameters;
 
 pub struct Perceptron {
-    pub weights: Vec<f64>,
-    pub bias: f64,
+    pub parameters: PerceptronParameters,
     pub activation: Box<dyn Activation>,
     pub weight_optimizers: Vec<Box<dyn Optimizer>>,
     pub bias_optimizer: Box<dyn Optimizer>,
@@ -17,9 +17,9 @@ impl Perceptron {
 
     fn weighted_sum(&self, inputs: &[f64]) -> f64 {
         inputs.iter()
-            .zip(self.weights.iter())
+            .zip(self.parameters.current.weights.iter())
             .map(|(x, w)| x * w)
             .sum::<f64>()
-            + self.bias
+            + self.parameters.current.bias
     }
 }
